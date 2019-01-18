24c170d9f06c05c3e80d6b9eafdd71ee14bd08f0
Moose <moose@posteo.de>
2019-01-18 13:22:00 +0100

    A Blocking retriever as a counterpart to the fiber retriever. Interface is the same, copy and paste, quick and easy to be able to use both types in the same templates

---
+++ BlockingRetriever.cpp
@@ -0,0 +1,16 @@
+
+//  Copyright 2018 Stephan Menzel. Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include "BlockingRetriever.hpp"
+
+namespace moose {
+namespace mredis {
+
+#if defined(BOOST_MSVC)
+void BlockingRetrievergetRidOfLNK4221() {}
+#endif
+
+}
+}
---
+++ BlockingRetriever.hpp
@@ -0,0 +1,147 @@
+
+//  Copyright 2019 Stephan Menzel. Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+#include "MRedisConfig.hpp"
+#include "MRedisResult.hpp"
+#include "MRedisTypes.hpp"
+
+#include <boost/thread.hpp>
+#include <boost/optional.hpp>
+#include <boost/cstdint.hpp>
+
+#include "tools/Assert.hpp"
+
+#include <string>
+#include <chrono>
+#include <atomic>
+
+namespace moose {
+namespace mredis {
+
+/*! @brief behavioral helper to allow for easier use of a the result future to wait for the result of an async op
+	blocking the current thread
+	@see FiberRetriever
+ */
+template <typename Retval>
+class BlockingRetriever {
+
+	public:
+		BlockingRetriever() = delete;
+		BlockingRetriever(const BlockingRetriever &) = delete;
+
+		/*! @brief initialize a new getter for one-time usage
+			@param n_timeout wait for this long before exception is returned 
+		 */
+		BlockingRetriever(const unsigned int n_timeout = 3)
+				: m_timeout(n_timeout)
+				, m_promise(new boost::promise< boost::optional<Retval> >)
+				, m_used(false) {
+		}
+
+		/*! @brief call either that or get the future
+			@return value if set, otherwise none
+			@throw redis_error
+		 */
+		boost::optional<Retval> wait_for_response() {
+			
+			MOOSE_ASSERT_MSG((!m_used), "Double use of get_result_in_fiber object");
+
+			using namespace moose::tools;
+
+			boost::unique_future< boost::optional<Retval> > future_value = m_promise->get_future();
+
+			// Now I think this wait_for would imply a yield... Meaning that other fiber will take over while this one waits
+			if (future_value.wait_for(boost::chrono::seconds(m_timeout)) == boost::future_status::timeout) {
+				m_used.store(true);
+				BOOST_THROW_EXCEPTION(redis_error() << error_message("Timeout getting redis value"));
+			}
+
+			m_used.store(true);
+
+			// Now we must have a value of correct type as our callback already checked for that.
+			// This may still throw however
+			return boost::get<Retval>(future_value.get());
+		}
+
+		//! @brief use this as a callback in AsyncClient calls
+		Callback responder() const {
+
+			static_assert(false, "Non MRedis answer type cannot be used with this template");
+		}
+
+	private:
+		const unsigned int     m_timeout;
+		std::unique_ptr< boost::promise< boost::optional<Retval> > > m_promise;
+		std::atomic<bool>      m_used;  //!< to prevent double usage of this object, set to true after use
+};
+
+template<>
+inline Callback BlockingRetriever<std::string>::responder() const {
+
+	return [this](const RedisMessage &n_message) {
+
+		using namespace moose::tools;
+
+		// translate the error into an exception that will throw when the caller get()s the future
+		if (is_error(n_message)) {
+			redis_error ex = boost::get<redis_error>(n_message);
+			this->m_promise->set_exception(std::make_exception_ptr(ex));
+			return;
+		}
+
+		if (is_null(n_message)) {
+			this->m_promise->set_value(boost::none);
+			return;
+		}
+
+		if (!is_string(n_message)) {
+			redis_error ex;
+			ex << error_message("Unexpected return type, not a string");
+			ex << error_argument(n_message.which());
+			this->m_promise->set_exception(std::make_exception_ptr(ex));
+			return;
+		}
+
+		this->m_promise->set_value(boost::get<std::string>(n_message));
+	};
+}
+
+template<>
+inline Callback BlockingRetriever<boost::int64_t>::responder() const {
+
+	return [this](const RedisMessage &n_message) {
+
+		using namespace moose::tools;
+
+		// translate the error into an exception that will throw when the caller get()s the future
+		if (is_error(n_message)) {
+			redis_error ex = boost::get<redis_error>(n_message);
+			this->m_promise->set_exception(std::make_exception_ptr(ex));
+			return;
+		}
+
+		if (is_null(n_message)) {
+			this->m_promise->set_value(boost::none);
+		}
+
+		if (!is_int(n_message)) {
+			redis_error ex;
+			ex << error_message("Unexpected return type, not an int");
+			ex << error_argument(n_message.which());
+			this->m_promise->set_exception(std::make_exception_ptr(ex));
+			return;
+		}
+
+		this->m_promise->set_value(boost::get<boost::int64_t>(n_message));
+	};
+}
+
+#if defined(BOOST_MSVC)
+MREDIS_API void BlockingRetrievergetRidOfLNK4221();
+#endif
+
+}
+}
